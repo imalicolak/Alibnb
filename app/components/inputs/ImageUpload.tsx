a45969990d58e5f30bb16f9ad8aa6e86b356fd3d
Usernameimalicolak <19acolak99@gmail.com>
2023-06-21 08:31:22 -0500

    Image uploader is good

---
+++ app/components/inputs/ImageUpload.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useCallback } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
+
+declare global {
+  let cloudinary: any;
+}
+
+interface ImageUploadProps {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
+  const handleUpload = useCallback(
+    (result: any) => {
+      onChange(result.info.secure_url);
+    },
+    [onChange]
+  );
+  // What do dependency arrays do exactly?
+
+  return (
+    <CldUploadWidget
+      onUpload={handleUpload}
+      uploadPreset="Alibnb"
+      options={{ maxFiles: 1 }}
+    >
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            className="relative cursor-pointer hover:opacity-50 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600"
+          >
+            <TbPhotoPlus size={50} />
+            <div className="font-semibold text-lg">Click to upload!</div>
+            {value && (
+              <div className="absolute inset-0 w-full h-full">
+                <Image
+                  alt="Upload"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  src={value}
+                />
+              </div>
+            )}
+          </div>
+        );
+      }}
+    </CldUploadWidget>
+  );
+};
+
+export default ImageUpload;
